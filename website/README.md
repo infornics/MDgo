eb8f08c8091bd639be2aabf3a08494ef7f0b1366
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2026-01-02 17:53:12 +0530

    chore: initial frontend setup

---
+++ website/README.md
@@ -0,0 +1,24 @@
+
+# website
+
+This project was created using `@rachitbharadwaj/create-next-app`. It includes:
+- Next.js with TypeScript and Tailwind CSS
+- shadcn preconfigured
+- Custom project structure and starter components
+
+## Getting Started
+
+1. Install dependencies:
+   ```
+   npm install
+   ```
+
+2. Run the development server:
+   ```
+   npm run dev
+   ```
+
+Visit http://localhost:3000 to view your project.
+
+Happy coding!
+    
---
+++ website/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "MDgo",
+    template: "%s | MDgo",
+  },
+  description:
+    "MDgo - View, edit, preview, convert to pdf and share your markdown files.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className}`}>{children}</body>
+    </html>
+  );
+}
